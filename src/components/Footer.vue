50a97d99cd64c05811be3de27f02a51e0c5bffd2
Nikita Gorobets <sawwa.cwer13@gmail.com>
2025-06-09 00:48:53 +0300

    Added Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,9 +6,68 @@
 </script>
 
 <template>
-
+  <footer class="footer">
+    <div class="container">
+      <div class="footer__inner">
+        <div class="footer__content">
+          <div class="footer__text">
+            <p class="footer__disclaimer">
+              ИМЕЮТСЯ ПРОТИВОПОКАЗАНИЯ.<br>
+              НЕОБХОДИМА КОНСУЛЬТАЦИЯ СПЕЦИАЛИСТА
+            </p>
+            <p class="footer__copyright">
+              © 2025 ООО «ГК РТ-Клиника» ИНН 1102334455
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+@use '../assets/scss/abstracts/variables' as *;
+@use '../assets/scss/abstracts/mixins' as *;
+.footer {
+  padding: 70px 0 46px 0;
+  // --- ИЗМЕНЕНИЯ ЗДЕСЬ ---
+  background-color: $light-gray;      // Фон изменен на светлый
+  color: $primary-color;            // Цвет текста изменен на темный
+  // ----------------------
 
+  @include responsive($breakpoint-tablet) {
+    padding: 37px 0 41px 0;
+  }
+  @include responsive($breakpoint-mobile) {
+    padding: 45px 0 41px 0;
+  }
+
+  &__text {
+    display: grid;
+    gap: 4px;
+
+    @include responsive($breakpoint-tablet) {
+      gap: 13px;
+    }
+    @include responsive($breakpoint-mobile) {
+      gap: 2px;
+    }
+  }
+
+  &__disclaimer {
+    letter-spacing: 5px;
+
+    @include responsive($breakpoint-mobile) {
+      font-size: rem(10px);
+    }
+  }
+
+  &__copyright {
+    font-size: rem($font-size-sm);
+
+    @include responsive($breakpoint-mobile) {
+      font-size: rem(8px);
+    }
+  }
+}
 </style>
