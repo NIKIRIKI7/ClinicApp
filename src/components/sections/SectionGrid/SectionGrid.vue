1633eed7952083ce75e7d24ea1c33ae03260155d
Nikita Gorobets <sawwa.cwer13@gmail.com>
2025-06-11 14:14:02 +0300

    Added SectionGrid

---
+++ src/components/sections/SectionGrid/SectionGrid.vue
@@ -0,0 +1,68 @@
+<script
+    setup
+    lang="ts"
+>
+import SectionGridItem from './SectionGridItem.vue';
+import { useContentStore } from '@/stores/content';
+
+/**
+ * @component SectionGrid
+ * @description Контейнер для отображения навигационной сетки.
+ */
+const contentStore = useContentStore();
+const gridItems = contentStore.sectionGridItems;
+</script>
+
+<template>
+  <section class="section-grid">
+    <div class="container">
+      <ul class="section-grid__list">
+        <li
+            v-for="item in gridItems"
+            :key="item.id"
+            class="section-grid__item"
+        >
+          <SectionGridItem :item="item" />
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+@use '../../../assets/scss/abstracts/variables' as *;
+@use '../../../assets/scss/abstracts/mixins' as *;
+
+.section-grid {
+  padding: rem(25) 0;
+  background-color: $secondary-color;
+
+  @include responsive($breakpoint-tablet) {
+    padding: rem(35) 0;
+  }
+  @include responsive($breakpoint-mobile) {
+    padding: rem(15) 0;
+  }
+}
+
+.section-grid__list {
+  display: grid;
+  gap: rem(30);
+
+  list-style: none;
+  padding: 0;
+  margin: 0;
+
+  grid-template-columns: repeat(4, 1fr);
+
+  @include responsive($breakpoint-tablet) {
+    grid-template-columns: repeat(3, 1fr);
+    gap: rem(20);
+  }
+
+  @include responsive($breakpoint-mobile) {
+    grid-template-columns: 1fr;
+    gap: rem(15);
+  }
+}
+</style>
---
+++ src/components/sections/SectionGrid/SectionGridItem.vue
@@ -0,0 +1,81 @@
+<script setup lang="ts">
+import type { PropType } from 'vue';
+import type { SectionGridItemData } from '@/stores/content';
+
+/**
+ * @component SectionGridItem
+ * @description Отображает одну визуальную карточку в навигационной сетке.
+ */
+defineProps({
+  item: {
+    type: Object as PropType<SectionGridItemData>,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="section-grid-card">
+    <span class="section-grid-card__text">{{ item.text }}</span>
+    <div class="section-grid-card__icon-wrapper">
+      <component
+          :is="item.icon"
+          class="section-grid-card__icon"
+          aria-hidden="true"
+      />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../../../assets/scss/abstracts/variables' as *;
+@use '../../../assets/scss/abstracts/mixins' as *;
+
+.section-grid-card {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: $white;
+  border-radius: rem(24);
+  padding: rem(10, 24);
+  box-shadow: 0 rem(4) rem(15) rgba(0, 0, 0, 0.05);
+  height: rem(90);
+
+  @include responsive($breakpoint-tablet) {
+    height: rem(85);
+    padding: rem(10, 18);
+  }
+
+  &__text {
+    @include apply-font($font-family-primary, $font-weight-semibold, 14px);
+    color: $text-color-dark;
+    text-transform: uppercase;
+    line-height: 1.3;
+    margin-right: rem(16);
+
+    @include responsive($breakpoint-mobile) {
+      // Убираем боковой отступ и добавляем нижний
+      margin-right: 0;
+    }
+  }
+
+  &__icon-wrapper {
+    @include flex-center;
+    flex-shrink: 0;
+    width: rem(60);
+    height: rem(60);
+    background-color: $accent-color;
+    border-radius: rem(14);
+
+    @include responsive($breakpoint-mobile) {
+      margin-left: 0;
+    }
+  }
+
+  &__icon {
+    width: rem(36);
+    height: rem(36);
+    color: $white;
+  }
+}
+</style>
